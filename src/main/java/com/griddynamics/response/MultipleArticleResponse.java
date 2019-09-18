d3029d5f2857cd94ded5b2eb08eed1e9b97e6ad7
jjaniak <julia.janiak@gmail.com>
2019-09-18 18:40:59 +0200

    remove constructor that shouldn't be there

--- src/main/java/com/griddynamics/response/MultipleArticleResponse.java
+++ src/main/java/com/griddynamics/response/MultipleArticleResponse.java
@@ -3,11 +3,6 @@ package com.griddynamics.response;
 import java.util.List;
 
 public class MultipleArticleResponse {
-    private List <Article> articles;
-    private int articlesCount;
-
-    public MultipleArticleResponse(List<Article> articles, int articlesCount) {
-        this.articles = articles;
-        this.articlesCount = articlesCount;
-    }
+    public List <Article> articles;
+    public int articlesCount;
 }
