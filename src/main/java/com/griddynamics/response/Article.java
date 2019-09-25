6dd365931691e9814a1c50b296c49c1c4ea38e54
jjaniak <julia.janiak@gmail.com>
2019-09-25 18:56:11 +0200

    fix typo and remove unnecessary annotation

--- src/main/java/com/griddynamics/response/Article.java
+++ src/main/java/com/griddynamics/response/Article.java
@@ -1,10 +1,7 @@
 package com.griddynamics.response;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.sql.Timestamp;
 import java.util.List;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
 
 public class Article {
     public String slug;
@@ -13,7 +10,7 @@ public class Article {
     public String body;
     public List <String> tagList;
     public Timestamp createdAt;
-    public Timestamp updatetAt;
+    public Timestamp updatedAt;
     public boolean favorited;
     public int favoritesCount;
     public Author author;
