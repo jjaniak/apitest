306500d21c900f6a21ee87c3f7e76ed4ab15be56
jjaniak <julia.janiak@gmail.com>
2019-09-30 19:54:37 +0200

    add todo for later

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -8,21 +8,21 @@ import org.junit.jupiter.api.AfterEach;
 public class BaseTest {
     protected static final String BASE_URI = "https://conduit.productionready.io/api";
 
-    @AfterEach
-    @Attachment(value = "Response", type = "application/json")
-    public String attachResponse(Object response) {
-        return response.toString();    // or prettyPrint instead of toString
-
-        //todo make both attachments work
-
-        // put adding attachments in AfterEach?
-        // type of response and requests?  RestAssuredResponse?
-    }
-
-    @Attachment(value = "Request", type = "application/json")
-    public String attachRequest(Object request) {
-        return request.toString();
-
-//      I only get object reference (but I should get body)  use RespectSpecification?
-    }
+//    @AfterEach
+//    @Attachment(value = "Response", type = "application/json")
+//    public String attachResponse(Object response) {
+//        return response.toString();    // or prettyPrint instead of toString
+//
+//        //todo make attachments work again
+//
+//        // put adding attachments in AfterEach?
+//        // type of response and requests?  RestAssuredResponse?
+//    }
+//
+//    @Attachment(value = "Request", type = "application/json")
+//    public String attachRequest(Object request) {
+//        return request.toString();
+//
+////      I only get object reference (but I should get body)  use RespectSpecification?
+//    }
 }
